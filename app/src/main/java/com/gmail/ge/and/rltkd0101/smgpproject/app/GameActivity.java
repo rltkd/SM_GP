c579b128a2f959145f81edc7d778a6fa554fd1b5
Baek Gisang <rltkd0101@gmail.com>
2025-05-07 17:52:53 +0900

    게임뷰 생성

--- app/src/main/java/com/gmail/ge/and/rltkd0101/smgpproject/app/GameActivity.java
+++ app/src/main/java/com/gmail/ge/and/rltkd0101/smgpproject/app/GameActivity.java
@@ -2,19 +2,16 @@ package com.gmail.ge.and.rltkd0101.smgpproject.app;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-import com.gmail.ge.and.rltkd0101.smgpproject.R;
+import com.gmail.ge.and.rltkd0101.smgpproject.a2dg.framework.scene.Scene;
 
-public class Game extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        new Scene().push();
     }
 }
