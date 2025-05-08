9dd66f9cd9137a4de8ca83251d69b3243f60ed52
Baek Gisang <rltkd0101@gmail.com>
2025-05-08 16:32:41 +0900

    리드미 가독성 개편

--- app/src/main/java/com/gmail/ge/and/rltkd0101/smgpproject/app/MainScene.java
+++ app/src/main/java/com/gmail/ge/and/rltkd0101/smgpproject/app/MainScene.java
@@ -7,20 +7,18 @@ import com.gmail.ge.and.rltkd0101.smgpproject.a2dg.framework.view.Metrics;
 
 class MainScene extends Scene {
     public enum Layer {
-        bg, Player,UI;// 배경
-        // 다른 레이어...
+        bg, Player,UI;
         public static final int COUNT = values().length;
     }
 
     public MainScene() {
         initLayers(Layer.COUNT);
 
-        // 배경 이미지 Sprite 추가
         add(Layer.bg, new Sprite(
                 R.mipmap.background1,
                 Metrics.width / 2, Metrics.height / 2,
                 Metrics.width, Metrics.height
-        ));
+        )); 
         Player player = new Player(Player.WeaponType.HANDGUN);
         add(Layer.Player, player);
         Joystick joystick = new Joystick(player);
