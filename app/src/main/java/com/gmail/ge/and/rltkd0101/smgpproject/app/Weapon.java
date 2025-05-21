bb0563af09f06f02a1709fa7bacab9d47b1e1dd0
Baek Gisang <rltkd0101@gmail.com>
2025-05-21 16:00:20 +0900

    weapon 인터페이스 구분

---
+++ app/src/main/java/com/gmail/ge/and/rltkd0101/smgpproject/app/Weapon.java
@@ -0,0 +1,7 @@
+package com.gmail.ge.and.rltkd0101.smgpproject.app;
+
+import com.gmail.ge.and.rltkd0101.smgpproject.a2dg.framework.scene.Scene;
+
+public interface Weapon {
+    void attack(Player player , Scene scene);
+}
