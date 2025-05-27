fb757586b123425b9767c1e3db2f613fb033d2e0
Baek Gisang <rltkd0101@gmail.com>
2025-05-27 15:47:07 +0900

    인터페이스 구분을 위한 추가

--- app/src/main/java/com/gmail/ge/and/rltkd0101/smgpproject/app/Weapon.java
+++ app/src/main/java/com/gmail/ge/and/rltkd0101/smgpproject/app/Weapon.java
@@ -5,6 +5,12 @@ import android.graphics.RectF;
 import com.gmail.ge.and.rltkd0101.smgpproject.a2dg.framework.scene.Scene;
 
 public interface Weapon {
-    void attack(Player player , Scene scene);
+    void attack(Player player, Scene scene);
     RectF getAttackBox(Player player);
+    int getSpriteResId();      // 무기별 애니메이션 시트
+    float getBaseDamage();     // 무기별 공격력
+    float getCooldown();       // 무기 쿨다운
+
+    int getFrameCount(); // 🔄 무기별 애니메이션 프레임 수
 }
+
